a34a46c96dfe87f0224d3f37a7ede8e301658820
t_ishikawa <tissi0708@gmail.com>
2022-09-28 09:16:58 +0900

    add go.mod in ch1

--- ch1/main.go
+++ ch1/main.go
@@ -6,4 +6,10 @@ func main() {
 	fmt.Println("Hello world!")
 	name := string([]byte{72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33, 10})
 	fmt.Println(name)
+
+	jsonText := "{\"Title\":\"Hello Golang\",\"Author\":\"Taro\",\"Year\":2022}"
+	fmt.Println(jsonText)
+
+	jsonTextBytes := []byte(jsonText)
+	fmt.Println(jsonTextBytes)
 }
